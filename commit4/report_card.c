be2efad5eb485fb9a32000220a5c6034425134d8
wjdgustn <wjdgustn15@naver.com>
2019-03-22 19:26:52 +0900

    report card: Print grades of each subject

---
+++ commit4/report_card.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	
+int kor, eng, math;
+
+	
+math = 80;
+eng = 100;
+	
+kor = 90;
+
+	printf("wjdguistn\n")
+printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
+	return 0;
+}
